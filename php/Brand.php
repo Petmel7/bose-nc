0c79c87f46ec44d48b6ab500fa19acbe1ce77c4c
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-12-20 18:16:44 +0200

    add

--- php/Brand.php
+++ php/Brand.php
@@ -1,19 +1,32 @@
 <section class="container">
-    <h2 class="general-title">BRAND CREATION HISTORY</h2>
-    <p class="general-text">The history of Bose Corporation began with disappointment. In 1956, MIT graduate student Amar Bose bought one of the best stereo systems at the time for his home. After a few minutes of listening to the audio recording, his bewilderment turned to disappointment. </p>
-    <div class="brand-img"></div>
-    <p class="general-text">From Amar's point of view, the sound was so flat and unnatural that it was better not to listen to the music at all than to listen to it in such a distorted form.</p>
-    <p class="general-text">Bose's frustration grew into a firm determination to build audio equipments that would reproduce sound with impeccable accuracy and naturalness</p>
-    <div class="brand-img--two"></div>
+    <article class="brand-article">
+        <div>
+            <h2 class="brand-title general-title">BRAND CREATION HISTORY</h2>
+            <p class="brand-title--text general-text">The history of Bose Corporation began with disappointment. In 1956, MIT graduate student Amar Bose bought one of the best stereo systems at the time for his home. After a few minutes of listening to the audio recording, his bewilderment turned to disappointment. </p>
 
-    <div class="brand">
-        <a href="#" class="">
-            <svg class="brand-svg">
-                <use href="./images.svg/symbol-defs.svg#icon-untitled"></use>
-            </svg>
-        </a>
+            <p class="general-text">From Amar's point of view, the sound was so flat and unnatural that it was better not to listen to the music at all than to listen to it in such a distorted form.</p>
+        </div>
+
+        <div>
+            <div class="brand-img"></div>
+            <p class="general-text">Bose's frustration grew into a firm determination to build audio equipments that would reproduce sound with impeccable accuracy and naturalness</p>
+        </div>
+    </article>
+
+    <div class="brand-block">
+        <div class="brand-img--two"></div>
+
+        <div>
+            <div class="brand">
+                <a href="#" class="">
+                    <svg class="brand-svg">
+                        <use href="./images.svg/symbol-defs.svg#icon-untitled"></use>
+                    </svg>
+                </a>
+            </div>
+
+            <h3 class="general-title--small">Never stop dreaming. Always dream of the best, and think about how to achieve it.</h3>
+            <p class="brand-text">Amar Bose is a company founder </p>
+        </div>
     </div>
-
-    <h3 class="general-title--small">Never stop dreaming. Always dream of the best, and think about how to achieve it.</h3>
-    <p class="brand-text">Amar Bose is a company founder </p>
 </section>
--- php/Reviews.php
+++ php/Reviews.php
@@ -5,19 +5,19 @@
         <div class="reviews-block">
             <iframe class="reviews-video" src="https://www.youtube.com/embed/VqP11Ma_2Fk?si=sfzHEIALDhQH-97M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <h3 class="reviews-text--red general-title--small">Bose Сorporation</h3>
-            <p class="general-text">Critically acclaimed for their powerful noise cancelling, astonishing sound, and unrivaled voice pickup, Bose Noise Cancelling Headphones 700 help turn any space into the perfect place to listen to music, get work done, or just shut out the world for a few moments and relax </p>
+            <p class="reviews-text general-text">Critically acclaimed for their powerful noise cancelling, astonishing sound, and unrivaled voice pickup, Bose Noise Cancelling Headphones 700 help turn any space into the perfect place to listen to music, get work done, or just shut out the world for a few moments and relax </p>
         </div>
 
         <div class="reviews-block">
             <iframe class="reviews-video" src="https://www.youtube.com/embed/BUyLpf-u97Y?si=-hFKgWrvZZvHfjjy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <h3 class="reviews-text--red general-title--small">Bose Сorporation</h3>
-            <p class="general-text">Bose Noise Cancelling Headphones 700 deliver everything you expect — and things you never imagined possible. The perfect balance of quiet, comfort, and sound </p>
+            <p class="reviews-text general-text">Bose Noise Cancelling Headphones 700 deliver everything you expect — and things you never imagined possible. The perfect balance of quiet, comfort, and sound </p>
         </div>
 
         <div class="reviews-block">
             <iframe class="reviews-video" src="https://www.youtube.com/embed/NgtznBDFRUo?si=vt9XjiPDscALGLOJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             <h3 class="reviews-text--red general-title--small">Bose Сorporation</h3>
-            <p class="general-text">Real stories of people tuning in or out to feel peace, find escape, and for self-care </p>
+            <p class="reviews-text general-text">Real stories of people tuning in or out to feel peace, find escape, and for self-care </p>
 
         </div>
 
